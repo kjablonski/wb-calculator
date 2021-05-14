3f598fe5f1c6c31637dc2a244208038ea77bcf54
kjablonski <kevin.m.jablonski@gmail.com>
2021-05-13 22:04:56 -0500

    SW change for cacheing

--- sw.js
+++ sw.js
@@ -22,15 +22,13 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
+    caches.open('mysite-dynamic').then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
           return response;
-        } else {
-          return fetch(event.request);
-		}
-      }
-    )
+        });
+      });
+    })
   );
 });
