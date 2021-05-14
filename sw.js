7cc2ae84585981932df217f8f7d27a5de3c5454e
kjablonski <kevin.m.jablonski@gmail.com>
2021-05-13 22:08:56 -0500

    fix static cache name

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
